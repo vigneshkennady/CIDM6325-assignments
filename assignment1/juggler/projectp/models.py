f34d04c1db7d41b091c4a5d9f1500f121dad6ce9
Vigneshwaran Kennady <vignesh@VignesharansMBP.attlocal.net>
2023-11-14 18:06:41 -0600

    assignment 1 submission

---
+++ assignment1/juggler/projectp/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    due_date = models.DateField()
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
